37e0075ff7f6678437f3240e9a4cc0bea0beb023
LYF1999 <850097832@qq.com>
2016-08-31 23:06:45 +0800

    fix
    fix

--- agentauth/models.py
+++ agentauth/models.py
@@ -10,4 +10,4 @@ class Agent(models.Model):
     mobile_phone = models.CharField('手机号', max_length=255)
     supervisor = models.CharField('上级名称', max_length=255)
     date = models.DateField('授权日期', null=True)
-    auth_no = models.CharField('授权号码', max_length=255)
+    auth_no = models.CharField('授权号码', max_length=255, unique=True)
--- command/views.py
+++ command/views.py
@@ -6,24 +6,28 @@ from django.utils import timezone
 
 # Create your views here.
 def command(request):
-    authorize = request.POST.get('authorize')
-    wechat = request.POST.get('wechat')
-    mobile_phone = request.POST.get('phone')
-    supervisor = request.POST.get('supervisor')
-    errors = check(authorize=authorize, phone=mobile_phone, wechat=wechat, supervisor=supervisor)
-    if errors:
-        Agent.objects.create(name=authorize,
-                             wechat_no=wechat,
-                             mobile_phone=mobile_phone,
-                             supervisor=supervisor,
-                             date=timezone.now()
-                             )
-    return render(request, 'command.html', {
-        'errors': errors,
-    })
+    if request.method == "GET":
+        return render(request, 'command.html')
+    else:
+        authorize = request.POST.get('authorize')
+        wechat = request.POST.get('wechat')
+        mobile_phone = request.POST.get('phone')
+        supervisor = request.POST.get('supervisor')
+        auth_no = request.POST.get('auth_no')
+        errors = check(authorize=authorize, phone=mobile_phone, wechat=wechat, supervisor=supervisor, auth_no=auth_no)
+        if errors:
+            Agent.objects.create(name=authorize,
+                                 wechat_no=wechat,
+                                 mobile_phone=mobile_phone,
+                                 supervisor=supervisor,
+                                 date=timezone.now()
+                                 )
+        return render(request, 'command.html', {
+            'errors': errors,
+        })
 
 
-def check(authorize, wechat, phone, supervisor):
+def check(authorize, wechat, phone, supervisor, auth_no):
     errors = {}
     if not authorize:
         errors.setdefault('authorize_error', u'兹授权为空,请填写')
@@ -33,6 +37,10 @@ def check(authorize, wechat, phone, supervisor):
         errors.setdefault('wechat_error', u'微信号为空, 请填写')
     if Agent.objects.filter(wechat_no=wechat).exists():
         errors.setdefault('wechat_error', u'微信号重复, 请重新填写')
+    if not auth_no:
+        errors.setdefault('auth_no_error', u'授权号码为空,请填写')
+    if Agent.objects.filter(auth_no=auth_no).exists():
+        errors.setdefault('auth_no_error', u'授权号码重复,请重新填写')
     if not phone:
         errors.setdefault('phone_error', u'手机号为空, 请填写')
     if not supervisor:
