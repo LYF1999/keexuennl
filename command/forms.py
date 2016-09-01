3e60623d515b28c22273355d60c77b44268f6ed2
LYF1999 <850097832@qq.com>
2016-09-01 12:33:17 +0800

    增加date选择插件
    增加date选择插件

--- command/forms.py
+++ command/forms.py
@@ -4,6 +4,8 @@ from django import forms
 
 
 class Authorize(forms.ModelForm):
+    date = forms.CharField(widget=forms.TextInput({
+    }))
     class Meta:
         model = Agent
         fields = ('name', 'wechat_no', 'mobile_phone', 'supervisor', 'date', 'auth_no')
