64d46ec27b613f1f887a020dbdf75f72fc2815e1
LYF1999 <850097832@qq.com>
2016-10-08 23:35:31 +0800

    fix
    fix

--- agentauth/models.py
+++ agentauth/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import datetime
 
 
 class Agent(models.Model):
@@ -11,5 +12,18 @@ class Agent(models.Model):
     mobile_phone = models.CharField('手机号', max_length=255, unique=True)
     supervisor = models.CharField('上级名称', max_length=255)
     start_date = models.DateField('授权日期', null=True)
-    end_date = models.DateField('结束日期', null=True)
+    end_date = models.DateField('结束日期', null=True, blank=True)
     auth_no = models.CharField('授权号码', max_length=255, unique=False, blank=True, null=True)
+
+    def get_code(self):
+        code = Agent.objects.order_by('-id').all()[0].id + 1
+        while Agent.objects.filter(auth_no=code).exists():
+            code += 1
+        return code
+
+    def save(self, *args, **kwargs):
+        if self.end_date is None or self.end_date == "":
+            self.end_date = datetime.date(self.start_date.year + 1, self.start_date.month, self.start_date.day)
+        if self.auth_no is None or self.auth_no == "":
+            self.auth_no = self.get_code()
+        super(Agent, self).save(*args, **kwargs)
